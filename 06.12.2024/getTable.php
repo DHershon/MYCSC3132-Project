0dcae7264c597d449b1021ed93a8e977794208f2
David HERSHON <139619511+DHershon@users.noreply.github.com>
2024-12-08 08:10:55 +0000

    chnages added

--- 06.12.2024/getTable.php
+++ 06.12.2024/getTable.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>STUDENT LIST</title>
+	<title>Student List</title>
 </head>
 <body>
 
 	<?php
 //get the db connection file
 
-require_once 'dbconf.php'; //(conf/dbconf.php) (folder/file)
-require_once 'myfun.php';
+require_once 'dbconf.php';
+require_once 'myfunc.php';
 
 //PrintTable ("student",$connect);
 student($connect);
