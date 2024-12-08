31f060e6b687f52936f0e6c9b1a0452626888a6d
David HERSHON <139619511+DHershon@users.noreply.github.com>
2024-12-08 08:18:36 +0000

    changes added

--- 06.12.2024/myfunc.php
+++ 06.12.2024/myfunc.php
@@ -2,50 +2,7 @@
 <?php
 //get the database connection file
 require_once 'dbconf.php';
-/*function PrintTable($tableName,$connect){
-try{
-	//Query
-	$sql = "SELECT * FROM $tableName";
 
-	//execute the query (call variable,query)
-	$result = mysqli_query($connect,$sql);
-
-	if (mysqli_num_rows($result)>0) {
-		//fetch the data from rows
-
-		echo "<table border=1> ";
-
-	$col = mysqli_fetch_fields($result);
-	//print the column
-	echo "<tr>";
-	foreach ($col as $value) {
-		//return an object
-		//print_r($value)
-			echo "<td>".$value->name."</td>";
-		}
-		echo "</tr>";
-		while($row = mysqli_fetch_assoc($result)){
-			//print the data in table format
-
-		echo "<tr>";
-		foreach ($row as $key => $value) {
-			echo "<td>$value</td>";
-		}
-		echo "</tr>";
-		}
-		echo "</table>";
-	} 
-	else{
-		echo "No results"; //table is empty
-	}
-
-	}
-
-
-catch(Exception $e){
-	die($e->getMessage());
-}
-}*/
  //GET DATA FROM DB
  function student($connect){
             try{
@@ -141,48 +98,4 @@ catch(Exception $e){
             }
         }
 
-       /* 
-	Join the tables
-       function Jointable($EMP_ID,$connect){
-            try{
-            
-                $sql = "SELECT * FROM students INNER JOIN enrollments ON students.student_id = enrollments.student_id;
-            
-            
-                $result = mysqli_query($connect,$sql);
-            
-                if (mysqli_num_rows($result)>0) {
-                
-            
-                    echo "<table border=1> ";
-            
-                $col = mysqli_fetch_fields($result);
-            
-                echo "<tr>";
-                foreach ($col as $value) {
-                    
-                        echo "<td>".$value->name."</td>";
-                    }
-                    echo "</tr>";
-                    while($row = mysqli_fetch_assoc($result)){
-                        
-                    echo "<tr>";
-                    foreach ($row as $key => $value) {
-                        echo "<td>$value</td>";
-                    }
-                    echo "</tr>";
-                    }
-                    echo "</table>";
-                } 
-                else{
-                    echo "No results"; 
-                }
-            
-                }
-            
-            
-            catch(Exception $e){
-                die($e->getMessage());
-            }
-        }*/
 ?>
