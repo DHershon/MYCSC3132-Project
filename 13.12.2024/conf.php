51a6d876f3547ed9247169d3f09424b22264780e
David HERSHON <139619511+DHershon@users.noreply.github.com>
2024-12-16 17:51:50 +0000

    changes added

--- 13.12.2024/conf.php
+++ 13.12.2024/conf.php
@@ -1,8 +1,9 @@
 <?php
-define('HOST', 'localhost');
-define('USERNAME', 'root');
-define('PWD', '');
-define('DB', 'icae02');
+define('SERVERNAME','127.0.0.1:3306');
+define('USERNAME','root');
+define('PASSWORD','mariadb');
+define('DNAME','icae02');
 
-$connection = mysqli_connect(HOST,USERNAME,PWD,DB);
+
+$connection = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DNAME);
 ?>
